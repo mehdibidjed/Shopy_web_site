b60ac9f15639e493d30652b2de1e46348dffb69e
MehdiBidjedCS <mehdi.zerbita@univ-constantine2.dz>
2025-02-02 00:36:57 +0100

    prepare work space

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+
+  return (
+    <>
+   <p className='w-full h-[200px] bg-red-600'> app</p> 
+    </>
+  )
+}
+
+export default App
